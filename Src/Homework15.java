e4c6900da69a5f664644927fab78fabb0462f882
Nikasan1 <nikasan4@gmail.com>
2023-10-26 14:33:22 +0200

    First commit

---
+++ Src/Homework15.java
@@ -0,0 +1,9 @@
+public class Homework15 {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        Person.personInfoStatic("Will", "Smith", "New York", 293672945);
+        person.personInfo("Jackie", "Chan", "Shanghai", 1231241241);
+        person.personInfo("Sherlock", "Holmes", "London", 377421235);
+    }
+}
